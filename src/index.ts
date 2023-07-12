3dc8a76f1e52ac0598652942f73976972c09c929
Asya Vee <hello@asyavee.me>
2023-07-12 22:37:04 +0400

    boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-require("dotenv").config();
-
+import { Bot, Context } from "grammy";
 import commands from "./commands";
 import bot from "./core/bot";
-import { development, production } from "./utils/launch";
+
+const launchBot = async (bot: Bot<Context>): Promise<void> => {
+    try {
+        await bot.api.setWebhook(`${process.env.VERCEL_URL}/api/index`);
+        console.log(`[SERVER] Bot starting webhook`);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const DEVELOPMENT_longPollBot = async (bot: Bot<Context>): Promise<void> => {
+    try {
+        await bot.api.deleteWebhook();
+        console.log("[SERVER] Bot starting polling");
+        await bot.start();
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 bot.use(commands);
 
-process.env.NODE_ENV === "development" ? development(bot) : production(bot);
+process.env.NODE_ENV === "development" ? DEVELOPMENT_longPollBot(bot) : launchBot(bot);
 
 export {};
